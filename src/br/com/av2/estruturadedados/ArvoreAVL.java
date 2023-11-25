f4ef575eef5ac96b788b6cb815fba9370a78c733
12114814 <willerhenrique100@gmail.com>
2023-11-24 21:37:36 -0300

    feat: excluir Main, implementar a main na classe ArvoreAVL

--- src/br/com/av2/estruturadedados/ArvoreAVL.java
+++ src/br/com/av2/estruturadedados/ArvoreAVL.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ArvoreAVL {
+    private Node root;
+
     public class Node {
         private Node left, right, parent;
         private int height = 1;
@@ -290,4 +292,76 @@ public class ArvoreAVL {
         }
 
     }
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        int escolha;
+
+        ArvoreAVL tree = new ArvoreAVL();
+
+        do {
+
+            System.out.println("<1> Inserir");
+            System.out.println("<2> Excluir");
+            System.out.println("<3> Pesquisar");
+            System.out.println("<4> Caminhamento em pré-ordem");
+            System.out.println("<5> Sair");
+            System.out.print("Escolha uma opção: ");
+
+            escolha = ler.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.println("Opção 1 selecionada: Inserir a partir do arquivo");
+
+                    // Modifique o caminho do arquivo conforme necessário
+                    String caminhoArquivo = "C:\\Users\\12114814\\IdeaProjects\\dados.txt";
+
+                    try {
+                        File arquivo = new File(caminhoArquivo);
+                        Scanner scannerArquivo = new Scanner(arquivo);
+
+                        while (scannerArquivo.hasNextLine()) {
+                            int valorInserir = Integer.parseInt(scannerArquivo.nextLine());
+
+                            // Chama o método insert para inserir o valor na árvore
+                            tree.root = tree.insert(tree.root, valorInserir);
+                        }
+
+                        scannerArquivo.close();
+
+                        System.out.println("Valores do arquivo inseridos com sucesso!");
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+                    }
+                    break;
+
+
+                case 2:
+                    System.out.println("Opção 2 selecionada: Excluir");
+                    // Implemente a lógica de exclusão aqui
+                    break;
+                case 3:
+                    System.out.println("Opção 3 selecionada: Pesquisar");
+                    // Implemente a lógica de pesquisa aqui
+                    break;
+                case 4:
+                    System.out.println("Opção 4 selecionada: Caminhamento em pré-ordem");
+                    // Implemente a lógica de caminhamento em pré-ordem aqui
+                    break;
+                case 5:
+                    System.out.println("Saindo do programa. Até mais!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+
+        } while (escolha != 5);
+
+        ler.close();
+    }
 }
+
+
